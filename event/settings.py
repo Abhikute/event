278e78cbd511317b69ebe2b1e812f46f37aa3a33
Kute <akute@ptc.com>
2019-12-14 02:13:57 +0530

    changes

--- event/settings.py
+++ event/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'import_export',
     'event_reg',
-    'crispy_forms',
+  
 ]
 
-CRISPY_TEMPLATE_PACK='BOOTSTRAP4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
